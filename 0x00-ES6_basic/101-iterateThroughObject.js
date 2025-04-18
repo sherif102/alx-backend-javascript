9d1230e80737ea8c8ec4eeecaeb07e5cf2d82959
hyperbayse <hyperbayse@gmail.com>
2025-04-18 18:37:18 +0100

    string slice

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-
+  let result = '';
+  for (const name of reportWithIterator) {
+    result += `${name} | `;
+  }
+  return result.slice(0, result.length - 3);
 }
