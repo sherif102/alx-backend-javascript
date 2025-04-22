fdb34cda06e604edd34aebb5d3182862802639fe
hyperbayse <hyperbayse@gmail.com>
2025-04-22 10:52:46 +0100

    correct import

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,5 @@
-import { uploadPhoto, createUser } from "../utils.js";
+import { uploadPhoto, createUser } from "utils.js";
 
 export default function handleProfileSignup() {
-  const all = Promise.all([uploadPhoto(), createUser()]);
-  all.then((response) => console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`));
-  all.catch((err) => console.error('Signup system offline', err));
+  return Promise.all([uploadPhoto(), createUser()]).then((response) => console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`)).catch((err) => console.error('Signup system offline', err));
 }
