1d69393ab665175601dbddea8b8b15948aa34d09
hyperbayse <hyperbayse@gmail.com>
2025-04-22 09:19:39 +0100

    correction so it return

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }));
-  promise.catch(() => (Error()));
-  promise.finally(() => {
-    console.log('Got a response from the API');
-  });
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => (new Error())).finally(() => console.log('Got a response from the API'));
 }
