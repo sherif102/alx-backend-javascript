45af842c01f81963b0e5442e93e304e953d81b9d
hyperbayse <hyperbayse@gmail.com>
2025-04-22 08:01:07 +0100

    correction

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  const promise = new Promise((resolve) => {
+  const promise = new Promise((resolve, reject) => {
     if (success === true) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      throw new Error('The face API is not working currently');
+      reject(Error('The fake API is not working currently'));
     }
   });
   return promise;
