1d87e75a06ab152c49601648028e4b8b7f87e85f
hyperbayse <hyperbayse@gmail.com>
2025-04-24 22:04:22 +0100

    correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  // const result = [];
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
   const promises = [user, photo];
-  // for (const promise of promises) {
-  //   if (promise.then()) {
-  //     result.push({ status: promise, value: promise.then((resolve) => resolve) });
-  //   } else {
-  //     result.push({ status: promise, value: promise.catch((err) => err) });
-  //   }
-  // }
   return Promise.allSettled(promises).then((results) => results);
 }
