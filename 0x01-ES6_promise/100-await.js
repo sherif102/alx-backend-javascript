c7ec779213f9a7be752a9dccbf36fd3cfeffc60a
hyperbayse <hyperbayse@gmail.com>
2025-04-25 11:24:40 +0100

    file import correction

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from '../utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   return Promise.allSettled([uploadPhoto(), createUser()]).then((results) => {
