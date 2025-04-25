78575600aa8c08125934f2f224d0a13d93dfa8e2
hyperbayse <hyperbayse@gmail.com>
2025-04-25 11:22:09 +0100

    correction

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,14 +2,11 @@ import { uploadPhoto, createUser } from '../utils';
 
 export default async function asyncUploadUser() {
   return Promise.allSettled([uploadPhoto(), createUser()]).then((results) => {
-    const returnObject = {};
     for (const result of results) {
       if (result.status === 'rejected') {
         return ({ photo: null, user: null });
       }
     }
-    returnObject.photo = results[0].value;
-    returnObject.user = results[1].value;
-    return returnObject;
+    return ({ photo: results[0].value, user: results[1].value });
   });
 }
