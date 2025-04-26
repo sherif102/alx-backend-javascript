c7d91f38c7205b10d49d02894eea7122fc19681b
hyperbayse <hyperbayse@gmail.com>
2025-04-26 12:35:36 +0100

    5-building.js

---
+++ 0x02-ES6_classes/test.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    return;
+  }
+}
