5f0ae23c4e5bb0afda52b851a360e84f7e6bf077
hyperbayse <hyperbayse@gmail.com>
2025-04-26 12:51:00 +0100

    correction

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,10 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
@@ -8,8 +12,6 @@ export default class Building {
   }
 
   evacuationWarningMessage() {
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    return;
   }
 }
--- 0x02-ES6_classes/test.js
+++ 0x02-ES6_classes/test.js
@@ -15,3 +15,21 @@ export default class Building {
     return;
   }
 }
+
+
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
