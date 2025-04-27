a722b25da1c371a30e8b453c3c18bbffd65b7017
hyperbayse <hyperbayse@gmail.com>
2025-04-27 14:53:04 +0100

    correction

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,7 +6,11 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
+  static get [Symbol.species]() {
+    return Car;
+  }
+
   cloneCar() {
-    return new super.constructor();
+    return new (this.constructor[Symbol.species])();
   }
 }
