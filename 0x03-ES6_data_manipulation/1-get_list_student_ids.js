e1419251d792d52eb7bbd5e10571411d13294e55
hyperbayse <hyperbayse@gmail.com>
2025-05-12 14:53:32 +0100

    correction

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(list) {
-  const returnList = [];
-  for (const item of list) {
-    if (item.id) returnList.push(item.id);
+  if (Array.isArray(list)) {
+    return list.map((item) => item.id);
   }
-  return returnList;
+  return [];
 }
---
+++ 0x03-ES6_data_manipulation/test.js
@@ -0,0 +1,7 @@
+const names = 'here';
+const arn = [1, 2];
+
+console.log(typeof names);
+console.log(typeof arn);
+console.log(Array.isArray(names));
+console.log(Array.isArray(arn))
