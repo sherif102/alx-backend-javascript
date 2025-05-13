f91d894750b32c852e6e88ecdd01feda378a0183
hyperbayse <hyperbayse@gmail.com>
2025-05-14 00:09:20 +0100

    correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
-
-  const filtered = Array.from(set).filter((value) => value.startsWith(startString));
-  const mapped = filtered.map((value) => value.slice(startString.length));
-  return mapped.join('-');
+  if (startString && typeof startString === 'string') {
+    const filtered = Array.from(set).filter((value) => value.startsWith(startString));
+    const mapped = filtered.map((value) => value.slice(startString.length));
+    return mapped.join('-');
+  }
+  return '';
 }
