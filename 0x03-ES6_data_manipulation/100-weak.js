ca05096c728f0030fd908bc4b179b00d03bc768f
hyperbayse <hyperbayse@gmail.com>
2025-05-14 01:59:22 +0100

    correction

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ const tracker = {};
 weakMap.set(tracker, 0);
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(tracker) >= 5) throw new Error('Endpoint load is high');
+  if (weakMap.get(tracker) >= 4) throw new Error('Endpoint load is high');
   const currentValue = weakMap.get(tracker);
   weakMap.set(tracker, currentValue + 1);
   weakMap.set(endpoint, currentValue + 1);
