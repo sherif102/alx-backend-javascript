20423666b08f2091c3ac9a29516bdfcb0f78be9c
hyperbayse <hyperbayse@gmail.com>
2025-05-14 01:55:06 +0100

    100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+export const weakMap = new WeakMap();
+const tracker = {};
+weakMap.set(tracker, 0);
+
+export function queryAPI(endpoint) {
+  if (weakMap.get(tracker) >= 5) throw new Error('Endpoint load is high');
+  const currentValue = weakMap.get(tracker);
+  weakMap.set(tracker, currentValue + 1);
+  weakMap.set(endpoint, currentValue + 1);
+}
--- 0x03-ES6_data_manipulation/test.js
+++ 0x03-ES6_data_manipulation/test.js
@@ -1,12 +1,4 @@
-const names = 'here';
-const arn = [1, 2];
-const data = { a: 'alif', b: 'baa' };
-
-console.log(typeof names);
-console.log(typeof arn);
-console.log(Array.isArray(names));
-console.log(Array.isArray(arn));
-
-data.c = 'taa';
-
-console.log(data);
+const names = new WeakMap();
+const obj = { tracker: 'one' };
+names.set(obj, 0);
+console.log(names.get(obj));
